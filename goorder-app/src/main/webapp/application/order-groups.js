9445b24fa38f5990ab41cb760f2a9949729040bf
Witold Szczerba <pljosh.mail@gmail.com>
2013-06-02 23:33:50 +0200

    ng-repeat on list items

--- goorder-app/src/main/webapp/application/order-groups.js
+++ goorder-app/src/main/webapp/application/order-groups.js
@@ -3,7 +3,13 @@ angular.module('order-groups', [])
 
   $scope.tables = [{
       label: 'Tivoli',
-      place: 'http://tivoli.demo'
+      place: 'http://tivoli.demo',
+      items: [
+        {id:1,who:'Witek',what:'Kurak',price:12.50},
+        {id:2,who:'Łukasz',what:'Makaron',price:10.50},
+        {id:3,who:'Zbyszek',what:'Makaron',price:10.50},
+        {id:4,who:'Krzysiek',what:'Rodzynki',price:5.60}
+      ]
     },{
       label: 'Chinol',
       place: 'http://chinol.demo'
